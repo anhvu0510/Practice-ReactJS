b31b5ba22f7065fb565a4a53a0082e6ef993b86d
vula <laanhvu99@gmail.com>
2022-11-27 00:19:47 +0700

    add format and restructure

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,15 @@
-import ToDoList from './components/ToDoList'
-import ToDoListReducer from './components/ToDoListReducer'
-import { StoreContext, useStore } from './store'
+import { ToDoList } from "./components/ToDoList";
+import { StoreContext, useStore } from "./store";
 // Sử dụng bình thường
 function App() {
-  const [state, dispatch] = useStore(StoreContext)
+  // const [state, dispatch] = useStore(StoreContext)
   return (
-    <div className='App'>
-      <ToDoListReducer />
+    <div className="App">
+      <h1>Todo APP</h1>
+      <ToDoList type="TODO" title="Việc cần làm" />
+      <ToDoList type="DONE" title="Việc đã hoàn thành" />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ todo-app/src/components/ToDoItem/ToDo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default React.memo(function ToDo({ id = '', name = '', onClickRemove = '' }) {
+    console.log(`Re-Render >>> ToDo: ${id}`);
+    return (
+        <li style={{ margin: '2px' }}>
+            {name}
+            <button onClick={onClickRemove} value={id} style={{ margin: '2px 10px' }}>
+                DONE
+            </button>
+        </li>
+    );
+});
